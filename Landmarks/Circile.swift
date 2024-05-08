9910756f17d97d54d1684e4a8c68e5fad12135a1
Esther Carrelle <esthcarelle@gmail.com>
2024-05-08 10:01:41 +0200

    Added texts and an image

---
+++ Landmarks/Circile.swift
@@ -0,0 +1,18 @@
+//
+//  Circile.swift
+//  Landmarks
+//
+//  Created by Esther Carrelle on 08/05/2024.
+//
+
+import SwiftUI
+
+struct Circile: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    Circile()
+}
